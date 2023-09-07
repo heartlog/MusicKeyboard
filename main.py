e7b3c1b4241cdd27858550f29785e9299abf15a7
Maday <76758776+heartlog@users.noreply.github.com>
2023-09-07 15:04:46 +0530

    modified:   main.py new file:   sounds/mixkit-arcade-retro-game-over-213.wav new file:   sounds/mixkit-crowd-laugh-424.wav new file:   sounds_release/mixkit-retro-game-notification-212.wav

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import os
+import random
 from pynput.keyboard import Key, Listener
 import winsound
-import random
 
-# List of available sounds
+# List of available sounds For onpress in "sounds"
 sounds_list_onpress = []
+# List of available sounds For onrelease in "sounds_release"
+sounds_list_onrelease = []
+
 
 def list_files(folder_path, sound_list):
     for filename in os.listdir(folder_path):
@@ -19,22 +22,32 @@ def play_random_sound(sounds):
     except Exception as e:
         print(f"Error playing sound: {e}")
 
-# 
 def on_press(key):
     if key == Key.esc:  # Add a key combination to exit the program (e.g., 'esc')
         return False  # Stop the listener
-    play_random_sound(sounds_list_onpress)
-
+    if is_empty(sounds_list_onpress) == False:
+        play_random_sound(sounds_list_onpress)
+    else:
+        print("There is no files | sounds folder is Empty.")
 
 def on_release(key):
-    pass  # You can add additional functionality for key releases here if needed
+    if is_empty(sounds_list_onrelease) == False:
+        play_random_sound(sounds_list_onrelease)
+    else:
+        pass
+
+def is_empty(alist):
+    return len(alist)==0
 
 
 def main():
+    # Load onpress sounds from "sounds"
     list_files("sounds", sounds_list_onpress)
-    print(sounds_list_onpress)
-    play_random_sound(sounds_list_onpress)
-    with Listener(on_press=on_press, on_release=on_release) as listener:
+    print("Loded OnPress files:",sounds_list_onpress)
+    # Load onrelease sounds from "sounds_release"
+    list_files("sounds_release", sounds_list_onrelease)
+    print("Loded OnRelease files:",sounds_list_onrelease)
+    with Listener(on_press=on_press, on_release=on_release) as listener:    # start Listener 
         print("Listening for key presses. Press 'esc' to exit.")
         listener.join()
 
